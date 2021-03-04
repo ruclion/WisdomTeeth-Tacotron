918152dd70db401637df5ebb4d66e50e5f24e375
hujk17 <hujk17@mails.tsinghua.edu.cn>
2021-03-04 17:18:52 +0800

    preprocessing完成, gl也完成

--- preprocess_dataset/preprocess.py
+++ preprocess_dataset/preprocess.py
@@ -2,64 +2,82 @@ import argparse
 import os
 from multiprocessing import cpu_count
 
-from hparams import hparams
-from tqdm import tqdm
-from datasets import ljspeech
-from datasets import databaker
-from datasets import multisets
+# from tqdm import tqdm
+import numpy as np
+import mel
+
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
 
 
-def write_metadata(metadata, out_dir):
-	with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
-		for m in metadata:
-			f.write('|'.join([str(x) for x in m]) + '\n')
-	mel_frames = sum([int(m[4]) for m in metadata])
-	timesteps = sum([int(m[3]) for m in metadata])
-	sr = hparams.sampling_rate
-	hours = timesteps / sr / 3600
-	print('Write {} utterances, {} mel frames, {} audio timesteps, ({:.2f} hours)'.format(
-		len(metadata), mel_frames, timesteps, hours))
-	print('Max input length (text chars): {}'.format(max(len(m[5]) for m in metadata)))
-	print('Max mel frames length: {}'.format(max(int(m[4]) for m in metadata)))
-	print('Max audio timesteps length: {}'.format(max(m[3] for m in metadata)))
+
+# in
+original_txt_path = '/ceph/dataset/DataBaker.BZNSYP/ProsodyLabeling/000001-010000.txt'
+wav_dir_path = '/ceph/dataset/DataBaker.DB6/CN/wav'
+
+# out
+mel_dir = os.path.join('training_data', 'mels')
+wav_dir = os.path.join('training_data', 'audio')
+lin_dir = os.path.join('training_data', 'linear')
+os.makedirs(mel_dir, exist_ok=True)
+os.makedirs(wav_dir, exist_ok=True)
+os.makedirs(lin_dir, exist_ok=True)
+out_txt_path = ['training_data/train.txt', 'training_data/val.txt', 'training_data/test.txt']
+
+
+
+
+def _process_utterance(mel_dir, linear_dir, wav_dir, index, wav_path, text):
+	if os.path.exists(wav_path):
+		mel_spectrogram, linear_spectrogram, out = mel.wav2mel(wav_path)
+		time_steps = len(out)
+		mel_frames = mel_spectrogram.shape[0]
+	else:
+		print('file {} present in csv metadata is not present in wav folder. skipping!'.format(wav_path))
+		return None
+
+	# Write the spectrogram and audio to disk
+	audio_filename = 'audio-{}.npy'.format(index)
+	mel_filename = 'mel-{}.npy'.format(index)
+	linear_filename = 'linear-{}.npy'.format(index)
+	np.save(os.path.join(wav_dir, audio_filename), out.astype(np.float32), allow_pickle=False)
+	np.save(os.path.join(mel_dir, mel_filename), mel_spectrogram.T, allow_pickle=False)
+	np.save(os.path.join(linear_dir, linear_filename), linear_spectrogram.T, allow_pickle=False)
+
+	# Return a tuple describing this training example
+	return (audio_filename, mel_filename, linear_filename, time_steps, mel_frames, text)
+
 
 
 def main():
-	print('initializing preprocessing..')
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--base_dir', default='')
-	parser.add_argument('--hparams', default='',
-		help='Hyperparameter overrides as a comma-separated list of name=value pairs')
-	parser.add_argument('--dataset', default='MultiSets')
-	parser.add_argument('--output', default='training_data')
-	parser.add_argument('--n_jobs', type=int, default=cpu_count())
-	args = parser.parse_args()
+	executor = ProcessPoolExecutor(max_workers=cpu_count())
+	futures = []
+	with open(original_txt_path, encoding='utf-8') as f:
+		# 000001	卡尔普#2陪外孙#1玩滑梯#4。
+		# ka2 er2 pu3 pei2 wai4 sun1 wan2 hua2 ti1
+		f_list = f.readlines()
+		i = 0
+		while i < len(f_list):
+			basename = f_list[i].strip()[:6]
+			wav_path = os.path.join(wav_dir_path, '{}.wav'.format(basename))
+			text = f_list[i + 1].strip()
+
+			futures.append(executor.submit(partial(_process_utterance, mel_dir, lin_dir, wav_dir, basename, wav_path, text)))
+			i += 2
+			# break
+
+		metadata = [future.result() for future in futures if future.result() is not None]
 	
-	# Prepare directories
-	# in_dir  = os.path.join(args.base_dir, args.dataset)
-	# out_dir = os.path.join(args.base_dir, args.output)
-	in_dir = args.base_dir
-	out_dir = args.output
-	mel_dir = os.path.join(out_dir, 'mels')
-	wav_dir = os.path.join(out_dir, 'audio')
-	lin_dir = os.path.join(out_dir, 'linear')
-	os.makedirs(mel_dir, exist_ok=True)
-	os.makedirs(wav_dir, exist_ok=True)
-	os.makedirs(lin_dir, exist_ok=True)
+	start = [0, 8000, 9000]
+	endd = [7999, 8999, 9999]
+	for k in range(3):
+		with open(out_txt_path[k], 'w', encoding='utf-8') as f:
+			for i in range(start[k], endd[k]):
+				f.write('|'.join([str(x) for x in metadata[i]]) + '\n')
 	
-	# Process dataset
-	if args.dataset == 'LJSpeech-1.1':
-		metadata = ljspeech.build_from_path(hparams, in_dir, mel_dir, lin_dir, wav_dir, args.n_jobs, tqdm=tqdm)
-	elif args.dataset == 'DataBaker':
-		use_prosody = False
-		metadata = databaker.build_from_path(hparams, in_dir, use_prosody, mel_dir, lin_dir, wav_dir, args.n_jobs, tqdm=tqdm)
-	elif args.dataset == 'MultiSets':
-		metadata = multisets.build_from_path(hparams, in_dir, mel_dir, lin_dir, wav_dir, args.n_jobs, tqdm=tqdm)
-	else:
-		raise ValueError('Unsupported dataset provided: {} '.format(args.dataset))
-	
-	# Write metadata to 'train.txt' for training
-	write_metadata(metadata, out_dir)
+	print('Write {} utterances'.format(len(metadata)))
+	print('Max input length (text chars): {}'.format(max(len(m[5]) for m in metadata)))
+	print('Max mel frames length: {}'.format(max(int(m[4]) for m in metadata)))
 
 
 if __name__ == '__main__':
