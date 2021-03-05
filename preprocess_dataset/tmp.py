5eeb746617eef85db0bee279cf0b7e3c6fea07ec
hujk17 <hujk17@mails.tsinghua.edu.cn>
2021-03-05 12:31:07 +0800

    tmp

---
+++ preprocess_dataset/tmp.py
@@ -0,0 +1,37 @@
+_pad = '_'
+_eos = '~'
+_tone = '123456'
+_letters = 'abcdefghijklmnopqrstuvwxyz'
+_space = ' '
+
+
+# Export all symbols:
+symbols = [_pad] + [_eos] + list(_tone) + list(_letters) + [_space]
+
+
+out_txt_path = ['training_data/train.txt', 'training_data/val.txt', 'training_data/test.txt']
+
+for path in out_txt_path:
+    f = open(path, encoding='utf-8')
+    out_list = []
+    i = 0
+    for x in f.readlines():
+        if i % 2 == 1:
+            continue
+        x = x.strip()
+        text = x.split('|')[5]
+        tag = True
+        for i in text:
+            if (i in symbols) is False:
+                tag = False
+                break
+        if tag:
+            out_list.append(x)
+        i += 1
+    
+    f.close()
+    f = open(path, 'w', encoding='utf-8')
+    for x in out_list:
+        f.write(x + '\n')
+
+
